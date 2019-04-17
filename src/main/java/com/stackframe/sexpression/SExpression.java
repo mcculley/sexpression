00b339e15d47fe693dbea07f424129c058388375
Gene McCulley <James.X.McCulley.-nd@disney.com>
2019-04-17 17:42:15 -0400

    Simplify.

--- src/main/java/com/stackframe/sexpression/SExpression.java
+++ src/main/java/com/stackframe/sexpression/SExpression.java
@@ -20,7 +20,7 @@ public class SExpression {
 
     private static Object parse(Reader r, AtomicInteger offset, boolean hasParent) throws IOException, ParseException {
         boolean quoted = false;
-        List<Object> currentList = new ArrayList<>();
+        List<Object> l = new ArrayList<>();
         StringBuilder currentAtom = new StringBuilder();
         int i;
         while ((i = r.read()) != -1) {
@@ -31,27 +31,27 @@ public class SExpression {
                     throw new ParseException("unexpected ( inside token starting with " + currentAtom, offset.get() - 1);
                 }
 
-                currentList.add(parse(r, offset, true));
+                l.add(parse(r, offset, true));
             } else if (c == ')') {
                 if (!hasParent) {
                     throw new ParseException("unexpected )", offset.get() - 1);
                 }
 
                 if (currentAtom.length() > 0) {
-                    currentList.add(currentAtom.toString());
+                    l.add(currentAtom.toString());
                 }
 
-                if (currentList.isEmpty()) {
+                if (l.isEmpty()) {
                     return Collections.emptyList();
                 } else {
-                    return Collections.unmodifiableList(currentList);
+                    return Collections.unmodifiableList(l);
                 }
             } else if (c == ' ' || c == '\n') {
                 if (quoted) {
                     currentAtom.append(c);
                 } else {
                     if (currentAtom.length() > 0) {
-                        currentList.add(currentAtom.toString());
+                        l.add(currentAtom.toString());
                         currentAtom = new StringBuilder();
                     }
                 }
@@ -64,10 +64,10 @@ public class SExpression {
             }
         }
 
-        if (currentList.isEmpty()) {
+        if (l.isEmpty()) {
             return currentAtom.toString();
         } else {
-            return Collections.unmodifiableList(currentList);
+            return Collections.unmodifiableList(l);
         }
     }
 
